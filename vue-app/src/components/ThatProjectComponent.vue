22efcd4f10e386acbecca1f92fa9ffd02ebe94ba
JeromePage <jerome.page.1@ulaval.ca>
2023-02-24 23:23:14 -0500

    Added custom component for that project

---
+++ vue-app/src/components/ThatProjectComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="that-project-component">
+    <h1>This is a special component for "This project"</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "ThatProjectComponent",
+  data() {
+    return {};
+  },
+};
+</script>
--- vue-app/src/views/SingleProjectView.vue
+++ vue-app/src/views/SingleProjectView.vue
@@ -5,11 +5,16 @@
     <div>{{ $t("projects." + id + ".name") }}</div>
     <p>{{ $t("projects." + id + ".description_1") }}</p>
     <p>{{ $t("projects." + id + ".description_2") }}</p>
+    <that-projet-component v-if="id === 'that-project'"></that-projet-component>
   </div>
 </template>
 <script>
+import ThatProjectComponent from "@/components/ThatProjectComponent.vue";
 export default {
   name: "SingleProject",
+  components: {
+    "that-projet-component": ThatProjectComponent,
+  },
   props: ["id"],
   data() {
     return {};
